8f756faa2167d7ef5951665c05a30a5f26113ca6
kurlee710 <josephatamaniuk@gmail.com>
2024-10-29 14:19:00 -0400

    created get all thoughts function

--- src/controllers/thoughtController.ts
+++ src/controllers/thoughtController.ts
@@ -1,3 +1,13 @@
 import Thought from "../models/Thought.js";
 import User from "../models/User.js";
 import { Request, Response } from "express";
+
+// Get all thoughts
+export const getThoughts = async (_: Request, res: Response) => {
+  try {
+    const thoughts = await Thought.find();
+    res.json(thoughts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
