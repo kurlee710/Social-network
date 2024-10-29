423b4ddaac8f38ec4bfc9f737e4462abc0552acd
kurlee710 <josephatamaniuk@gmail.com>
2024-10-29 14:06:18 -0400

    added get controller function

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,3 +1,13 @@
 import User from "../models/User.js";
 import Thought from "../models/Thought.js";
 import { Request, Response } from "express";
+
+// Get all users
+export const getUsers = async (_req: Request, res: Response) => {
+  try {
+    const users = await User.find().populate("friends thoughts");
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
