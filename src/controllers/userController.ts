213f00e51e4f316fadb1e440ee2012cd4228a20b
kurlee710 <josephatamaniuk@gmail.com>
2024-10-29 14:10:02 -0400

    added create user function

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -23,3 +23,13 @@ export const getUserById = async (req: Request, res: Response) => {
     res.status(500).json(err);
   }
 };
+
+// Create a user
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
