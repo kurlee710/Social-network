274d05b4aecaab10b743b008fd223f6285175439
kurlee710 <josephatamaniuk@gmail.com>
2024-10-29 14:01:31 -0400

    created reaction schema

--- src/models/Reaction.ts
+++ src/models/Reaction.ts
@@ -6,3 +6,31 @@ interface IReaction extends Document {
   username: string;
   createdAt: Date;
 }
+const reactionSchema = new Schema<IReaction>(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp: Date) => timestamp,
+    },
+  },
+  {
+    toJSON: { getters: true },
+    _id: false,
+  }
+);
+
+export default reactionSchema;
