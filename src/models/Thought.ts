2dcf0768de66925fd93d6b4c3df40cc76153924f
kurlee710 <josephatamaniuk@gmail.com>
2024-10-29 14:00:10 -0400

    created thought schema

--- src/models/Thought.ts
+++ src/models/Thought.ts
@@ -7,3 +7,38 @@ interface IThought extends Document {
   username: string;
   reactions: (typeof reactionSchema)[];
 }
+
+const thoughtSchema = new Schema<IThought>(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp: Date) => timestamp,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+export default Thought;
